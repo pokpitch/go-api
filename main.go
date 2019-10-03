79925ce6f5558ee2e997918532467071e7db289c
Pokpitch Patcharadamrongkul <pokpitch.patcharadamrongkul@stream.co.th>
2019-10-03 15:32:04 +0700

    added: logging

--- main.go
+++ main.go
@@ -1,12 +1,14 @@
 package main
 
 import (
+	"log"
 	"encoding/json"
 	"fmt"
 	"net/http"
 )
 
 func main() {
+	log.Print("Starting the service...")
 	handleRequest()
 }
 
@@ -27,6 +29,8 @@ func getAllAddressBook(w http.ResponseWriter, r *http.Request) {
 func handleRequest() {
 	http.HandleFunc("/", homePage)
 	http.HandleFunc("/address", getAllAddressBook)
+	
+	log.Print("The service is ready to listen and serve.")
 	http.ListenAndServe(":8080", nil)
 }
 
